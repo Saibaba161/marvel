b6333c0b4b612304f39394ec8057b5987df03ad3
Saibaba-1610 <sai1610k@gmail.com>
2024-07-25 12:11:34 +0530

    awesome css

--- src/pages/characters.js
+++ src/pages/characters.js
@@ -42,47 +42,39 @@ const CharacterData = () => {
     }
 
     return (
-        <div className="main-content">
-          <form onSubmit={handleSubmit} className="search-form">
-            <input 
-              placeholder="Who's your fav character"
-              onChange={(e) => setSearchQuery(e.target.value)}
-              value={searchQuery}
-              className="search-input"
-            />
-            <button type="submit" className="search-button">Search</button>
-          </form>
-      
-          <div className="character-grid">
-            {charData.results && charData.results.map((character) => (
-              <div key={character.id} className="character-card">
+      <div className="main-content">
+      <form onSubmit={handleSubmit} className="search-form">
+        <input 
+          placeholder="Who's your fav character"
+          onChange={(e) => setSearchQuery(e.target.value)}
+          value={searchQuery}
+          className="search-input"
+        />
+        <button type="submit" className="search-button">Search</button>
+      </form>
+    
+      {searchQuery && (
+        <div className="character-grid">
+          {charData.results && charData.results.map((character) => (
+            <div key={character.id} className="character-card">
+              <div className="character-image-container">
                 <img 
                   src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
                   alt={character.name}
                   className="character-image"
                 />
-
-                <h2 className="character-name">{character.name}</h2>
-                <p className="character-description">{character.description}</p>
-                
-                <div className="character-details">
-                <h3>Comics:</h3>
-                <p>Available: {character.comics.available}</p>
-                
-                <h3>Series:</h3>
-                <p>Available: {character.series.available}</p>
-                
-                <h3>Stories:</h3>
-                <p>Available: {character.stories.available}</p>
-                
-                
-                <h3>Events:</h3>
-                <p>Available: {character.events.available}</p>
+                {character.description && (
+                  <div className="character-description">
+                    <p>{character.description}</p>
+                  </div>
+                )}
               </div>
-              </div>
-            ))}
-          </div>
+              <h2 className="character-name">{character.name}</h2>
+            </div>
+          ))}
         </div>
+      )}
+    </div>
       );
 }
 
